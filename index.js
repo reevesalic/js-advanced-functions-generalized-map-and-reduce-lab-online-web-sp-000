548eb301a77971c39e4354a47937ceda8fb36ef1
reevesalic <reevesalic@gmail.com>
2021-01-25 11:55:46 -0500

    commit tree 47a13b9c5170eccd16236c69a59093d808b5c4cf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(source, call) {
+let x = []
+for (let i =0; i < source.length; i ++) {
+     let element = source[i]
+     x.push(call(element))
+}
+return x
+}
+
+function reduce(source, call, start) {
+     let x = (!!start) ? start : source[0]
+     let i = (!!start) ? 0 : 1
+     for (; i < source.length; i ++) {
+          x = call(source[i], x)
+     }
+     return x
+}
